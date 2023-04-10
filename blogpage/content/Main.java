e210ab47e655574dd79781e888b46fd6b734bbf4
yangfan6 <yangfan6@dgg.net>
2023-04-10 18:44:18 +0800

    更新 笔记

---
+++ blogpage/content/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
